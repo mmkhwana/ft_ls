483e1c02aca10d1e9f3417196fbafa45b00cf76a
Malibongwe Mduduzi Comfort MKHWANAZI <mmkhwana@c5r2s5.wethinkcode.co.za>
2019-09-16 09:07:00 +0200

    me

---
+++ ft_ls.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ls.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmkhwana <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/16 09:00:39 by mmkhwana          #+#    #+#             */
+/*   Updated: 2019/09/16 09:06:28 by mmkhwana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+#define FT_LS_H
+
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <uuid/uuid.h>
+#include <grp.h>
+#include <uuid/uuid.h>
+#include <time.h>
+#include "libft/libft.h"
+
+void		ft_permissions(struct stat d_stat);
+
+#endif
